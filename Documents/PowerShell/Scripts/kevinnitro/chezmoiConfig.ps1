8a87b2da01a23c58d8f7182cd8464cc53d8ac6b5
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-04-24 14:02:38 +0700

    2:02 PM on 24/4/24

--- Documents/PowerShell/Scripts/kevinnitro/chezmoiConfig.ps1
+++ Documents/PowerShell/Scripts/kevinnitro/chezmoiConfig.ps1
@@ -2,11 +2,16 @@
 function cms
 {
   Set-Location
+  if (!(Get-Process "gpg-agent" -ErrorAction SilentlyContinue))
+  {
+    gpg-connect-agent /bye
+  }
   chezmoi re-add
   Set-Location "$env:USERPROFILE/.local/share/chezmoi"
   git commit --message "$(Get-Date -Format 'h:mm tt on d/M/y')"
   git push
   # chezmoi git apply -R
+  Stop-Process "gpg-agent"
 }
 
 function Update-ChezmoiCompletion
