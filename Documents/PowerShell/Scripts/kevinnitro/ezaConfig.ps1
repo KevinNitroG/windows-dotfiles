e14333fcdd17fcda8ab342da5d42108167713661
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-04 08:47:47 +0700

    chore(pwsh): eza sort filetype

--- Documents/PowerShell/Scripts/kevinnitro/ezaConfig.ps1
+++ Documents/PowerShell/Scripts/kevinnitro/ezaConfig.ps1
@@ -1,7 +1,7 @@
 function _eza
 {
   # eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions $args
-  eza --long --git --icons=always --color=always $args
+  eza --long --git --icons=always --color=always --sort="type" $args
 }
 
 Set-Alias -Name ls -Value _eza -Force
