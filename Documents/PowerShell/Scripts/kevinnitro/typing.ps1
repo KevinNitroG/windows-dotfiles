94ecb45fab67b8d19c39e40869fd0257ccdf6ccd
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-11 13:50:48 +0700

    feat(pwsh): add EVKey start stop

--- Documents/PowerShell/Scripts/kevinnitro/typing.ps1
+++ Documents/PowerShell/Scripts/kevinnitro/typing.ps1
@@ -19,3 +19,25 @@ function Restart-Unikey
   Stop-Unikey
   Start-Unikey
 }
+
+function Stop-EVKey
+{
+  if (Get-Process -Name "EVKey64" -ErrorAction SilentlyContinue)
+  {
+    Start-Process -filepath "powershell" -Argumentlist "Stop-Process -Name EVKey64" -Verb runas
+  } else
+  {
+    Write-Host "EVKey64 isn't running! No need to stop!"
+  }
+}
+
+function Start-EVKey
+{
+  Start-Process "$($env:LOCALAPPDATA)\EVKey\EVKey64.exe" -Verb runas
+}
+
+function Restart-EVKey
+{
+  Stop-EVKey
+  Start-EVKey
+}
